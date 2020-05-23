3b7229a9de54db454ed961d9c359e3f840d69917
Binh Pham <binh1298@gmail.com>
2020-05-24 01:13:07 +0700

    Connect Auth & Users Modules

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
-      return result;
+    if (user && bcrypt.compareSync(password, user.password)) {
+      return user;
     }
     return null;
   }
