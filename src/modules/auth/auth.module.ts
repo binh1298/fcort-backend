c0183ce11d3ddb5a75da628c460da455861ae89c
Binh Pham <binh1298@gmail.com>
2020-05-24 04:51:04 +0700

    Implement environment var for jwt secret

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,17 +4,22 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
-
 import { CustomLocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'hehehe',
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '12h' },
+      })
     }),
   ],
   providers: [AuthService, CustomLocalStrategy, JwtStrategy],
