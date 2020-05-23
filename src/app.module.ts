a8ed008e2f9036e3fba71293f65ce414902f4a77
Binh Pham <binh1298@gmail.com>
2020-05-24 04:59:55 +0700

    Implement exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Module({
   imports: [
@@ -13,7 +15,9 @@ import { AuthModule } from './modules/auth/auth.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => ({
         type: 'postgres' as 'postgres',
         host: configService.get<string>('DB_HOSTNAME'),
         port: configService.get<number>('DB_PORT'),
@@ -22,13 +26,19 @@ import { AuthModule } from './modules/auth/auth.module';
         database: configService.get<string>('DB_DATABASENAME'),
         logging: true,
         autoLoadEntities: true,
-        synchronize: true
+        synchronize: true,
       }),
     }),
     UsersModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
