35222e7dfbd53967db9a8867267792c929bb03bd
Binh Pham <binh1298@gmail.com>
2020-05-31 22:38:29 +0700

    Init Favorite Groups Service

--- src/modules/groups/groups.module.ts
+++ src/modules/groups/groups.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupEntity } from 'src/entities/group.entity';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
+import { FavoriteGroupsService } from './groups.favorite.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([GroupEntity])],
   controllers: [GroupsController],
-  providers: [GroupsService],
+  providers: [GroupsService, FavoriteGroupsService],
 })
 export class GroupsModule {}
