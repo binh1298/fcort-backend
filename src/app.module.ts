b0b2cac16dbbf31302443e8216e21ff3f99dd9cf
Binh Pham <binh1298@gmail.com>
2020-06-02 22:08:54 +0700

    Re organize imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RouterModule, Routes } from 'nest-router';
+
+import { configService } from './config/config.service';
 
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { GroupsModule } from './modules/groups/groups.module';
+import { GroupsFavoritesModule } from './modules/groups-favorites/groups-favorites.module';
+import { MessagesModule } from './modules/messages/messages.module';
+
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ResponseDataInterceptor } from './interceptors/response-data.interceptor';
-import { GroupsModule } from './modules/groups/groups.module';
-import { configService } from './config/config.service';
-import { GroupsFavoritesModule } from './modules/groups-favorites/groups-favorites.module';
-import { MessagesModule } from './modules/messages/messages.module';
 
 const routes: Routes = [
   {
